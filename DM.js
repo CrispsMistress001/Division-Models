f634e25bf20b7088cbe17b471b916a3f50797e75
Kristaps Eisaks <kristapseisaks03@gmail.com>
2022-10-04 10:45:48 +0100

    Update 0.21
    More fixes with the user side + disabled SSL due to issues

--- DM.js
+++ DM.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const axios = require('axios').default;
-const fs = require('fs');
 const https = require('https');
 const bodyParser = require('body-parser');
 const app = express();
 const asyncHandler = require('express-async-handler')
 // to use my own functions
+
+
+const fs = require('fs'), readline = require("readline"); // file reader
+
 
 
 // SESSION AND COOCKIE CONTROL
@@ -17,6 +20,7 @@ const sessions = require('express-session');
 var sanitizer_string = require("string-sanitizer");
 const { callbackify } = require('util');
 const { resolve } = require('path');
+const { ppid } = require('process');
 
 
 
@@ -34,72 +38,121 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 // CERTIFICATES PART
 
-const privateKey = fs.readFileSync(__dirname+'/SSL/server.key', 'utf8');
-const certificate = fs.readFileSync(__dirname+'/SSL/server.cert', 'utf8');
-//const ca = fs.readFileSync('/etc/letsencrypt/live/DLS.com/chain.pem', 'utf8');
+// const privateKey = fs.readFileSync(__dirname+'/SSL/server.key', 'utf8');
+// const certificate = fs.readFileSync(__dirname+'/SSL/server.cert', 'utf8');
 
-const credentials = {
-    key: privateKey,
-    cert: certificate
-};
+
+// const credentials = {
+//     key: privateKey,
+//     cert: certificate
+// };
 
 // END OF CERTIFICATES
 
 
-async function Check_Login(Users,Password){
-    var userFound = false;
-    // This is to check whether they have inserted symbols or no
-    if(sanitizer_string.sanitize(Users) == Users && sanitizer_string.sanitize(Password)==Password){
-        // get sanitized results
-        Users = sanitizer_string.sanitize(Users);
-        Password = sanitizer_string.sanitize(Password);
+function Check_Login(Users,Password){
+    return new Promise((resolve,reject)=> {
 
-        
-        //var data = '';
-        //created a stream reader as it is better for peformance as it 
-        //uses less memory as it does not save the file into RAM before reading
-        const StreamReader = fs.createReadStream(__dirname+'/_users/Users.txt','utf8');
-        StreamReader.setEncoding('UTF8');
+        // This is to check whether they have inserted symbols or no
 
-        StreamReader.on('data', function(chunk) {
-            //data += chunk;
-            // check whether password and username is matching
-            console.log("Username|"+Users+"|password|"+Password);
-            if(chunk.split('|')[0] == Users && chunk.split('|')[1] == Password){
-                console.log("Found user");
-                userFound = true;
-                StreamReader.destroy(); // to stop the reader from continuing to save peformance
-            }
-        });
-        
-        StreamReader.on('end',function() {
-        });
+            var userFound = false;
 
-        StreamReader.on('error', function (error) {
-            console.log(`error: ${error.message}`);
-        });
+            const readInterface = readline.createInterface({
+                input: fs.createReadStream(__dirname+'/_users/Users.txt')
+            });
 
-        if(userFound){
-            return ("true");
+            readInterface.on('line', function(chunk) {
+                //console.log(chunk);
+                if(chunk.split('|')[0] == Users && chunk.split('|')[1] == Password){
+                    //console.log("Found user");
+                    userFound = true;
+                    readInterface.close(); // to stop the reader from continuing to save peformance
+                    readInterface.removeAllListeners()
+                }
+            });
 
-        }else{
-            return ("false");
-        }
-    }
-    else{
-        return ("Please make sure you have not inserted any symbols");
-    }
+            readInterface.on('end',function() {
+                if (userFound){
+                    resolve("true");
+                }else{
+                    resolve("false");
+                }
+            });
+
+            readInterface.on('close',function() {
+                if (userFound){
+                    resolve("true");
+                }else{
+                    resolve("false");
+                }
+            });
+
+            readInterface.on('error', function (error) {
+                console.log(`error: ${error.message}`);
+            });
+
+
+    });
 
 }
 
 
-app.get('/',(req,res)=>{
+
+function UserName_Found(Users){
+    return new Promise((resolve,reject)=> {
+
+        var userFound = false;
+
+        const readInterface = readline.createInterface({
+            input: fs.createReadStream(__dirname+'/_users/Users.txt'),
+            output: process.stdout,
+            console: false
+        });
+
+        readInterface.on('line', function(chunk) {
+            //data += chunk;
+            // check whether password and username is matching
+            //console.log("Username|"+Users+"|password|"+Password);
+            if(chunk.split('|')[0] == Users){
+                //console.log("Found user");
+                userFound = true;
+                readInterface.close(); // to stop the reader from continuing to save peformance
+                readInterface.removeAllListeners();
+            }
+        });
+        
+        readInterface.on('end',function() {
+            if (userFound){
+                resolve("true");
+            }else{
+                resolve("false");
+            }
+        });
+
+        readInterface.on('close',function() {
+            if (userFound){
+                resolve("true");
+            }else{
+                resolve("false");
+            }
+        });
+
+        readInterface.on('error', function (error) {
+            console.log(`error: ${error.message}`);
+        });
+
+
+    });
+}
+
+app.get('/',function(req,res){
     session=req.session;
     if(session.userid){
         res.send("Welcome User <a href=\'/logout'>click to logout</a>");
     }else{
         res.sendFile(__dirname + "/_web/index.html");
     }
+
 });
 
 app.post('/Login_Data_Send.html',async(req,res)=>{
@@ -113,28 +166,24 @@ app.post('/Login_Data_Send.html',async(req,res)=>{
             var Pass = req.body.Pass;
             //console.log(req.body.Name +"|"+req.body.Pass);
 
-            var returnVal = await Check_Login(UserName,Pass);
-            console.log("User check - "+returnVal);
-
-
-            if(returnVal == "true"){
-                session=req.session;
-                session.userid=UserName;
-                console.log(req.session);
-                res.send("Logged in");
-            }else if (returnVal== "false"){
-                res.send('Invalid username or password');
-            }else{
-                res.send(returnVal);
+            Check_Login(UserName,Pass).then((resp)=> {
                 
-            }
- 
-            //var returnVal =  Check_Login(UserName,Pass);
 
+                if(resp == "true"){
+                    session=req.session;
+                    session.userid=UserName;
+                    // console.log(req.session);
 
+                    res.send("Logged in!");
+                }else if (resp == "false"){
+                    res.send('Invalid username or password');
+                }else{
+                    res.send("Unkown error has occured");
+                }
+                console.log("User check - "+returnVal);
 
+            });
 
-            //res.header("Access-Control-Allow-Origin", "*").sendStatus(200);
         }
 
     } catch (error) {
@@ -142,7 +191,10 @@ app.post('/Login_Data_Send.html',async(req,res)=>{
     }
 });
 
-https.createServer(credentials, app).listen(3000,function(){
+// https.createServer(credentials, app).listen(3000,function(){
+//     console.log("Oi cunt server is running on port 3000");
+// });
+app.listen(3000,function(){
     console.log("Oi cunt server is running on port 3000");
 });
 
